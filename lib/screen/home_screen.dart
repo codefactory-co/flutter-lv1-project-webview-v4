e86f513e6e4778b2c79fccb160f7e41f41bcc028
Jiho Choi <withj1123@gmail.com>
2023-11-09 09:09:04 +0900

    version update

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller;
-  final homeUrl = 'https://blog.codefactory.ai';
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
 
   HomeScreen({Key? key}) : super(key: key);
 
@@ -19,11 +22,7 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {
               // http://
               // https://
-              if(controller == null){
-                return;
-              }
-
-              controller!.loadUrl(homeUrl);
+              controller.loadRequest(homeUrl);
             },
             icon: Icon(
               Icons.home,
@@ -31,13 +30,16 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: WebView(
-        onWebViewCreated: (WebViewController controller) {
-          this.controller = controller;
-        },
-        initialUrl: homeUrl,
-        javascriptMode: JavascriptMode.unrestricted,
+      body: WebViewWidget(
+        controller: controller,
       ),
+      // WebView(
+      //   onWebViewCreated: (WebViewController controller) {
+      //     this.controller = controller;
+      //   },
+      //   initialUrl: homeUrl,
+      //   javascriptMode: JavascriptMode.unrestricted,
+      // ),
     );
   }
 }
